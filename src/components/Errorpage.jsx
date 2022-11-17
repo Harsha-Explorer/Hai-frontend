1d42b0dd91988477616ceb3e652ccbdc16b52a4b
Harsha-Explorer <harshahv0120@gmail.com>
2022-11-17 16:32:48 +0530

    minor changes for error page

--- src/components/Errorpage.jsx
+++ src/components/Errorpage.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import '../App.css'
 import Navibar from './Navibar';
-
+import { useNavigate } from 'react-router-dom';
 const Errorpage = () => {
+  const navigate = useNavigate();
   return (
     <>
     <Navibar />
-    <div style={{'left':'50%','top':'50%','position':'absolute','transform':'translate(-50%, -50%)'}}>
+    <div fluid className='container text-center' style={{'marginTop':'15%','backgroundColor':'white','borderColor':'white'}}>
       <h1 className='text-center fw-bold mx-3 mb-0' style={{'textAlign':'center'}}>WE ARE SORRY PAGE NOT FOUND!</h1>
+      <br></br>
       <p className='text-center mx-3 mb-0' style={{'textAlign':'center'}}>The page you are looking for might have been removed, had it name changed
           or is temporarily unavailable
       </p>
-      <button className="btn btn-primary mx-3 w-10 text-center mb-0" to="/" >Back to home</button>
+      <br></br>
+      <button type="button" className="btn btn-primary text-center" onClick={()=>{navigate('/')}} >Back to home</button>
+      
     </div>
     </>
   )
